29e28cc74f26640f44182209a839e3e56e81e3c1
Nicole Kalbfleisch <ni.ka.50@hotmail.com>
2019-05-26 10:50:25 -0700

    minor setup for recycler view

---
+++ app/src/main/java/com/example/healthtracker/ExerciseLayoutAdapter.java
@@ -0,0 +1,8 @@
+package com.example.healthtracker;
+
+public class ExerciseLayoutAdapter extends RecyclerView.Adapter<ExerciseLayoutAdapter.ExerciseHolder> {
+
+
+    public static class ExerciseHolder extends RecyclerView.ViewHolder {
+    }
+}
