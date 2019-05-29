28f3715d2fc00dec202dcc7a564b1803682ad9d0
Nicole Kalbfleisch <ni.ka.50@hotmail.com>
2019-05-29 13:30:12 -0700

    built out retrofit, not quite working yet

--- app/src/main/java/com/example/healthtracker/ExerciseDiaryActivity.java
+++ app/src/main/java/com/example/healthtracker/ExerciseDiaryActivity.java
@@ -4,9 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.room.Room;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -20,6 +26,9 @@ import java.util.List;
 public class ExerciseDiaryActivity extends AppCompatActivity {
 
     public AppDatabase db;
+
+    TextView text;
+
     RecyclerView recyclerView;
     RecyclerView.LayoutManager layoutManager;
     ExerciseLayoutAdapter adapter;
@@ -28,6 +37,8 @@ public class ExerciseDiaryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise_diary_activiy);
+
+        this.text = findViewById(R.id.rest_view);
 
         db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "exercise-db")
                 .allowMainThreadQueries()
@@ -57,12 +68,38 @@ public class ExerciseDiaryActivity extends AppCompatActivity {
 
         Exercise exercise = new Exercise();
         exercise.setTitle(title.getText().toString());
-        exercise.setQuantity(quantity.getText().toString()); // TODO: change to int
+        exercise.setQuantity(quantity.getText().toString());
         exercise.setDescription(description.getText().toString());
         exercise.setTimestamp(time.getText().toString());
 
         db.exerciseDao().add(exercise);
+    }
 
+    public void onRetrofit(View view) {
+        String baseUrl = "localhost:8080/";
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        DiaryService service = retrofit.create(DiaryService.class);
+
+        service.getAllExercises().enqueue(new Callback<Exercise>() {
+            @Override
+            public void onResponse(Call<Exercise> call, Response<Exercise> response) {
+                text.setText(response.body().toString());
+            }
+
+            @Override
+            public void onFailure(Call<Exercise> call, Throwable t) {
+                Log.e("ERROR!!!", t.toString());
+            }
+        });
+
+        service.createExercise(
+                
+        )
     }
 
     public void goHome(View view) {
